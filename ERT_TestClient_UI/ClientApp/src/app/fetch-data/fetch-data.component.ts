fbd4cebf61ff4b2896c1db94a82936eccedb165d
kcampo1335 <42774916+kcampo1335@users.noreply.github.com>
2021-03-14 01:49:17 -0500

    Updated Metadata Dialog Page

--- ERT_TestClient_UI/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ERT_TestClient_UI/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,11 +1,18 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
+
+  getTextBoxVal(item) {
+ 
+  }
+  
+
   public forecasts: WeatherForecast[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
