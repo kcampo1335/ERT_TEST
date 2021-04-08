3684901599f3193ca3860019a80fdf36086e2250
kcampo1335 <42774916+kcampo1335@users.noreply.github.com>
2021-04-08 10:35:45 -0400

    Added Metadata Environment Pages

---
+++ ERT_TestClient_UI/ClientApp/src/app/metadata-environment/metadata-environment.component.ts
@@ -0,0 +1,14 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+
+
+@Component({
+  selector: 'app-metadata-environment-component',
+  templateUrl: './metadata-environment.component.html',
+  styleUrls: ["./metadata-environment.component.css"]
+})
+
+export class MetadataEnvironmentComponent {
+}
